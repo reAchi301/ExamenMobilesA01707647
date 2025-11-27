f1d282268ab9300576cb0db3d283e3d018699b2f
Ricardo Calzada <a01707647@tec.mx>
2025-11-27 15:37:43 -0600

    CovidStat.kt

--- app/src/main/java/com/app/examenmobilesa01707647/domain/model/CovidStat.kt
+++ app/src/main/java/com/app/examenmobilesa01707647/domain/model/CovidStat.kt
@@ -1,2 +1,9 @@
 package com.app.examenmobilesa01707647.domain.model
 
+data class CovidStat(
+    val country: String,
+    val region: String,
+    val date: String,
+    val totalCases: Int,
+    val newCases: Int
+)
