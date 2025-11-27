57b249e484fe0a5b2cba3b16b074d16ef6c846bc
Ricardo Calzada <a01707647@tec.mx>
2025-11-27 15:57:59 -0600

    CovidRepositoryImpl.kt created, this helps to implement the repository in Data, little reminder YOU CAN DO THIS ! BELIEVE IN YOURSELF

---
+++ app/src/main/java/com/app/examenmobilesa01707647/data/repository/CovidRepositoryImpl.kt
@@ -0,0 +1,23 @@
+package com.app.examenmobilesa01707647.data.repository
+
+import com.app.examenmobilesa01707647.data.remote.api.CovidApi
+import com.app.examenmobilesa01707647.data.remote.dto.toDomain
+import com.app.examenmobilesa01707647.domain.model.CovidStat
+import com.app.examenmobilesa01707647.domain.repository.CovidRepository
+import javax.inject.Inject
+
+class CovidRepositoryImpl @Inject constructor(
+    private val api: CovidApi
+) : CovidRepository {
+
+    override suspend fun getCovidStats(country: String): List<CovidStat> {
+        return try {
+            val response = api.getCovidData(country = country)
+            response.map { it.toDomain() }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            emptyList()
+        }
+    }
+}
+
