39d1b17ee8e102744c67ad8801ac9dc567dabbaa
Ricardo Calzada <a01707647@tec.mx>
2025-11-27 16:53:13 -0600

    Bug Fix in gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.app.examenmobilesa01707647"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.app.examenmobilesa01707647"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
