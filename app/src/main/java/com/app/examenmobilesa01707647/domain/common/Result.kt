6c15cb3f773e100d17c1443f5f7a635a8e616b81
Ricardo Calzada <a01707647@tec.mx>
2025-11-27 16:19:37 -0600

    Backend done, LoadingShimmer and result corrected, my bad, still can do it btw

--- app/src/main/java/com/app/examenmobilesa01707647/domain/common/Result.kt
+++ app/src/main/java/com/app/examenmobilesa01707647/domain/common/Result.kt
@@ -1,50 +1,7 @@
-package com.app.examenmobilesa01707647.presentation.common
+package com.app.examenmobilesa01707647.domain.common
 
-import androidx.compose.animation.core.*
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-
-@Composable
-fun LoadingShimmer(modifier: Modifier = Modifier) {
-    val shimmerColors = listOf(
-        Color.LightGray.copy(alpha = 0.6f),
-        Color.LightGray.copy(alpha = 0.2f),
-        Color.LightGray.copy(alpha = 0.6f),
-    )
-
-    val transition = rememberInfiniteTransition(label = "")
-    val translateAnim = transition.animateFloat(
-        initialValue = 0f,
-        targetValue = 1000f,
-        animationSpec = infiniteRepeatable(
-            animation = tween(1000),
-            repeatMode = RepeatMode.Restart,
-        ),
-        label = "",
-    )
-
-    val brush = Brush.linearGradient(
-        colors = shimmerColors,
-        start = Offset.Zero,
-        end = Offset(x = translateAnim.value, y = translateAnim.value),
-    )
-
-    Surface(
-        modifier = modifier,
-        shape = RoundedCornerShape(8.dp),
-    ) {
-        Spacer(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(brush),
-        )
-    }
+sealed class Result<out T> {
+    object Loading : Result<Nothing>()
+    data class Success<T>(val data: T) : Result<T>()
+    data class Error(val exception: Throwable) : Result<Nothing>()
 }
